406194f3192ad82e6580e49df3c1273dcbe0b544
Yoze Hariando <yoze.rejank@gmail.com>
2025-11-12 15:51:59 +0700

    Track dist for packages/embedding-atlas only

---
+++ packages/embedding-atlas/dist/component.js
@@ -0,0 +1,7 @@
+import { K as o, Q as s, Z as i, a as d } from "./chunk-BVOctT9v.js";
+export {
+  o as EmbeddingView,
+  s as EmbeddingViewMosaic,
+  i as defaultCategoryColors,
+  d as maxDensityModeCategories
+};
